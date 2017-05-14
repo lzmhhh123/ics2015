60a163a6c56fe6d72a402c7095e4d6acdab632ae
Zhuomin Liu <15307130223@fudan.edu.cn>
2017-05-14 18:11:33 +0800

    do something for test.

---
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -0,0 +1,16 @@
+#include "cpu/exec/template-start.h"
+
+#define instr test
+
+static void do_execute() {
+  cpu.CF = cpu.OF = 0;
+  DATA_TYPE result = op_src->val & op_dest->val;
+  update_EFLAGS_PZS(result);
+  print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+#include "cpu/exec/template-end.h"
