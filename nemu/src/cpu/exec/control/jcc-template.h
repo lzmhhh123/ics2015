d89a25f6d561d88a620f4c3b65849ae4767bd1a0
Zhuomin Liu <15307130223@fudan.edu.cn>
2017-05-20 12:49:50 +0800

    complete jcc.

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -38,5 +38,145 @@ static void do_execute() {
 make_instr_helper(i)
 #undef instr
 
+#define instr jo
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.OF == 1) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jno
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.OF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i);
+#undef instr
+
+#define instr jb
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.CF == 1) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jae
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.CF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jne
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.ZF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr ja
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.CF == 0 && cpu.ZF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr js
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.SF == 1) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jns
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.SF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jp
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.PF == 1) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jnp
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.PF == 0) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jl
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.SF != cpu.OF) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jge
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.SF == cpu.OF) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jle
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.ZF == 1 || cpu.SF != cpu.OF) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
+#define instr jg
+
+static void do_execute() {
+	get_new_eip();
+	if(cpu.ZF == 0 && cpu.SF == cpu.OF) cpu.eip = new_eip;
+}
+
+make_instr_helper(i)
+#undef instr
+
 #undef CODE_LEN
 #include "cpu/exec/template-end.h"
