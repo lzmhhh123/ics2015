aeaa15a3563618238e5b410034368e5710c8e5ab
Zhuomin Liu <15307130223@fudan.edu.cn>
2017-05-23 10:22:43 +0800

    improve and-template.h

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -7,8 +7,9 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	update_EFLAGS_PZS(result);
+	cpu.CF = cpu.OF = 0;
+	
 	print_asm_template2();
 }
 
